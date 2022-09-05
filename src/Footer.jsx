4833e67e8b9fa7cd61b74085211e418df1245b6d
unknown <aindovina2@yahoo.com>
2022-09-05 13:40:03 -0400

    fixed accessibility errors

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer>
       <div className="footer-wrap">
         <img className='logo' src={logo} alt="shortly logo" />
-        <nav aria-roledescription='footer navigation'>
+        <nav aria-roledescription='explore more navigation'>
           <ul className="footer-links list-styles">
             <li className='footer-link'><span>Features</span>
               <ul className='list-margin'>
